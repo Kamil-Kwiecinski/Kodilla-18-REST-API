66cfc74bd1b25ada3e5475a6b06c447b23d43471
Kamil Kwiecinski <kkwiecinski18@gmail.com>
2022-07-21 17:41:54 +0200

    23.4

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class Mail {
-    private final String mailTo;
+    private final String receiverEmail;
     private final String subject;
     private final String message;
     private final String toCc;
